78b2eca3867627aa316266dc03d767ef754691a6
webVueBlog <2979986590@qq.com>
2024-02-25 17:53:16 +0800

    实现分库分表 + 读写分离

---
+++ sub-db-table-read-write/src/main/java/com/da/Application.java
@@ -0,0 +1,17 @@
+package com.da;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description: 启动类
+ *
+ */
+@SpringBootApplication()
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
